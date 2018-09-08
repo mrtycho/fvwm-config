7fc71ab01d66d570b4625b69e21595e45acaa320
Stefan Mayer <mayer.langenau@gmail.com>
2018-09-08 19:58:04 +0200

    Wallpaper handling, updated decorations

--- scripts/colorthumb.sh
+++ scripts/colorthumb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ThumbDir=$HOME/.fvwm/.thumbs
+ThumbDir=$FVWM_USERDIR/.thumbs
 [ -d $ThumbDir ] || mkdir $ThumbDir
 
-Colors="tan peru sienna olivedrab midnightblue cadetblue steelblue slategrey grey30"
+Colors="tan peru sienna indianred olivedrab midnightblue cadetblue steelblue slategrey grey30 black"
 
 for col in $Colors;
 do
-	[ -d $ThumbDir/$col.png ] || convert $HOME/.fvwm/images/tile.png -fill $col -colorize 100 $ThumbDir/$col.png
+	[ -d $ThumbDir/$col.png ] || convert $FVWM_USERDIR/.tile.png -fill $col -colorize 100 $ThumbDir/$col.png
 
-	echo "+ \"$col%$ThumbDir/$col.png%\" Exec exec xsetroot -solid $col"
+	echo "+ \"$col%$ThumbDir/$col.png%\" Exec exec \`xsetroot -solid $col && echo \"xsetroot -solid $col\"  > $FVWM_USERDIR/background.cfg \`"
 done
